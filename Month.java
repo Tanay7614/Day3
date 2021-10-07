849e62f2ca564251aec6f2bae75f7f6288e57c85
Tanay Rathod <tanay7614.com>
2021-10-07 15:15:16 +0530

    Employed

---
+++ Month.java
@@ -0,0 +1,14 @@
+package Uc3;
+
+public class Month {
+	public static void main(String[] args)
+	{
+	 int working_day=20;
+	 int wage_per_hour=20;
+	 int working_hourper_day=9;
+	 int wage_per_day=working_hourper_day*wage_per_hour;
+	 int wage_month=  wage_per_day*working_day;
+	  System.out.println("wages for month="+wage_month);
+	}
+	}
+
