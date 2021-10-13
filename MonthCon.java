56d2f919ad2c02f3eb01ac7a6fbd8165b5802b10
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-13 14:13:47 +0530

    Month_wage  Using While Condition

--- MonthCon.java
+++ MonthCon.java
@@ -1,36 +1,39 @@
 package Uc6;
 
 public class MonthCon {
-	public static void main(String[] args)
-	{
-         int parttime_hour=1;
- 	  int full_time=2;
-	  int wage_perhour=20;
-          int emp_wage=0
-          int days=0;		  
-	  int total_working_days=20;
-	  while (days<=total_working_days)
-	{
-		 int w=(int)Math.floor(Math.random()*10)%3;
-                 switch (w) {
-			case parttime_hour: 
-				emp_hrs=4;
-				break;
+	int part_time=1;
+	int full_time=2;
+	static int parttime_hour=4;
+	static int full_timehour=8;
+	static int wage_per_hour=20;
+	static int no_of_working_days=20;
+	static int max_hrs_month=100;
+	public static void main(String[] args){
+		int days=0;
+		int total_emp_wage=0;
+		int emp_hrs=0;
 		
-      		       case full_time:
-                		emp_hrs=8;
-            		        break;
-
-    	               default:
-          		        System.out.println("Inavalid choice");
- 			  }
-		  emp_wage=emp_wage+emp_hrs*wage_perhour;
-		  days++;
+		//System.out.println(w);
+            while( days<no_of_working_days && emp_hrs<max_hrs_month ) {
+            double w=Math.floor(Math.random()*10)%3;
+		switch ((int)w) {
+		  case 1:
+		       int emp_wage=parttime_hour*wage_per_hour;
+		       System.out.println("Part time Employyed wage=" + emp_wage);
+		       total_emp_wage= total_emp_wage+emp_wage;
+		       emp_hrs=emp_hrs+parttime_hour;
+		       break;
+		  case 2:
+		      emp_wage=full_timehour*wage_per_hour;
+		      System.out.println("full time Employyed wage=" + emp_wage);
+		      total_emp_wage= total_emp_wage+emp_wage;
+		      emp_hrs=emp_hrs+full_timehour;
+		      break;
+                  default:
+		      System.out.println("Employed is Absent");
+		   }
+		days++;
+        }
+        System.out.println("Total Employed Wage for 20 Days "+ total_emp_wage);
 	}
-  		  System.out.println("Tatal wage of montnth=" + emp_wage);
-
-	  
-	}
-	
-	
 }
